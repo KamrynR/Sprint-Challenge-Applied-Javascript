c16f1c01a0b5c12dcd8dcb9398f1a134a46ef6ec
KamrynR <62856359+KamrynR@users.noreply.github.com>
2020-06-12 19:47:50 -0500

    pr

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,54 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        response.data.articles.javascript.forEach(article => {
+            cardCont.appendChild(cardMaker(article.headline, article.authorPhoto, article.authorName));
+        });
+        response.data.articles.bootstrap.forEach(article => {
+            cardCont.appendChild(cardMaker(article.headline, article.authorPhoto, article.authorName));
+        });
+        response.data.articles.jquery.forEach(article => {
+            cardCont.appendChild(cardMaker(article.headline, article.authorPhoto, article.authorName));
+        });
+        response.data.articles.node.forEach(article => {
+            cardCont.appendChild(cardMaker(article.headline, article.authorPhoto, article.authorName));
+        });
+        response.data.articles.technology.forEach(article => {
+            cardCont.appendChild(cardMaker(article.headline, article.authorPhoto, article.authorName));
+        });
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+function cardMaker(headline, imgURL, authorName) {
+    let card = document.createElement('div');
+    let cHeadline = document.createElement('div');
+    let cAuthor = document.createElement('div');
+    let cImgCont = document.createElement('div');
+    let cImg = document.createElement('img');
+    let cAuthorName = document.createElement('span');
+
+    card.classList.add('card');
+    cHeadline.classList.add('headline');
+    cAuthor.classList.add('author');
+    cImgCont.classList.add('img-container');
+
+    card.appendChild(cHeadline);
+    card.appendChild(cAuthor);
+    cAuthor.appendChild(cImgCont);
+    cAuthor.appendChild(cAuthorName);
+    cImgCont.appendChild(cImg);
+
+    cHeadline.innerHTML = headline;
+    cAuthorName.innerHTML = authorName;
+
+    cImg.setAttribute('src', imgURL);
+
+    return card;
+}
+
+let cardCont = document.querySelector('.cards-container');
